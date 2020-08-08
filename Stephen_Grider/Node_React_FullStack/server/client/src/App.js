a1c1e5fe4a781f393231355a6a58e85776a7a016
evolentDelight <evolentDelight@outlook.com>
2020-08-08 14:50:48 -0700

    Comprehension edits

--- Stephen_Grider/Node_React_FullStack/server/client/src/App.js
+++ Stephen_Grider/Node_React_FullStack/server/client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,15 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="/auth/google">
+          Sign in with Google
         </a>
-        <a href="http://localhost:5000/auth/google">Sign in with Google</a>
       </header>
     </div>
   );
--- Stephen_Grider/Node_React_FullStack/server/client/src/setupProxy.js
+++ Stephen_Grider/Node_React_FullStack/server/client/src/setupProxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require("http-proxy-middleware")
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
@@ -6,10 +6,19 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "http://localhost:5000",
     })
-  )
-}
+  );
+};
 
 /* Stephen Grider Node React Course
   Do not add proxy configuration
   Do not add changeOrigin: true to proxy script
 */
+
+/*
+  Create React App automatically looks for this file in src directory!
+  Uses the NodeJS format
+  Does not apply to production build
+
+  Logic Applied:
+  If React App's server initiates that link, then this middleware executes the proxy to localhost:5000
+  */
