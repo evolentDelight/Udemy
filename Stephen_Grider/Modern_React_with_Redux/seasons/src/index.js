a86db75960fdc11123c4cebeb6cb75e07adbd240
evolentDelight <evolentDelight@outlook.com>
2020-09-06 11:03:45 -0700

    Refactor to class components from functional react

--- Stephen_Grider/Modern_React_with_Redux/seasons/src/index.js
+++ Stephen_Grider/Modern_React_with_Redux/seasons/src/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hi There!</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+      },
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
+
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => {
+//       console.log(position); //Average time to fetch: 28ms=0.028s - PersonalC
+//     },
+//     (err) => console.log(err)
+//   ); //Takes a while to get data
+
+//   return <div>Latitude ${}</div>;
+// };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
