beb77a2432cbf0b170ac23898842691e5febf1ef
evolentDelight <evolentDelight@outlook.com>
2021-01-29 15:53:17 -0800

    {this} in terms of javascript and solutions

--- Stephen_Grider/Modern_React_with_Redux/pics/src/components/SearchBar.js
+++ Stephen_Grider/Modern_React_with_Redux/pics/src/components/SearchBar.js
@@ -3,11 +3,32 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit(event) {
+  //Fix #1
+  // onFormSubmit(event) {
+  //   event.preventDefault();
+  //By binding in constructor this.onFormSubmit = this.onFormSubmit.bind(this); the this.state.term can be called
+  //   //console.log(this.state.term);
+  //   //calling this.state.term causes error
+  //   //TypeError: Cannot read property 'state' of undefined
+  //   //this refers to parent object. In this example, it's onSubmit() in JSX <form>
+  //     //Essentially think onSubmit.(this.onFormSubmit); before the dot, the parent object
+  // }
+
+  //Fix #2
+  onFormSubmit = (event) => {
+    //Arrow functions(2015 implementation) automatically binds this to any expected references to this
     event.preventDefault();
 
-    //calling this.state.term causes error
-    //TypeError: Cannot read property 'state' of undefined
+    console.log(this.state.term);
+  };
+
+  //Fix #3
+  onFormSubmit(event) {
+    //After creating an anonymous arrow function in the JSX declaration
+    //onSubmit={(event) => this.onFormSubmit(event)} Pass event property
+    event.preventDefault();
+
+    console.log(this.state.term);
   }
 
   render() {
@@ -22,7 +43,6 @@ class SearchBar extends React.Component {
               placeholder="Enter Here"
               onChange={(e) => {
                 this.setState({ term: e.target.value });
-                console.log(this.state.term);
               }}
             />
           </div>
