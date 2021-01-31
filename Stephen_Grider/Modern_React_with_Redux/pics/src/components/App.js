693e6d3f193d9b6825f3922f357aa0a2b8fb84a6
evolentDelight <evolentDelight@outlook.com>
2021-01-30 16:41:13 -0800

    Axios Implementation & Pitfalls

--- Stephen_Grider/Modern_React_with_Redux/pics/src/components/App.js
+++ Stephen_Grider/Modern_React_with_Redux/pics/src/components/App.js
@@ -1,15 +1,28 @@
 import React from "react";
+import unsplash from "../api/unsplash.js";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    //Method Two - async&await
+    const response = await unsplash.get("search/photos", {
+      params: { query: term },
+    });
+    // Method One
+    // .then((response) => {
+    //   console.log(response.data.results);
+    // });
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
